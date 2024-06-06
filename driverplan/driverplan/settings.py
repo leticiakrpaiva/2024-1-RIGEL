eca1e4dfdb19cafe61e9c188d4b102eb7283891c
Mateus Villela Consorte <108163301+MVConsorte@users.noreply.github.com>
2024-06-06 20:22:50 -0300

    fix(docker)!: atualizar docker-compose e ajustar configurações
    - Recriar e modificar o arquivo docker-compose.yml para melhorar a orquestração dos contêineres.
    - Adicionar .gitignore e .dockerignore para excluir arquivos e diretórios desnecessários do versionamento e do build do Docker.
    - Criar venv para padronização (ambiente virtual com as variáveis de ambiente).
    - Incluir commands.sh para automatizar tarefas comuns.
    - Realizar correções necessárias em arquivos de configuração para garantir a integridade do ambiente Docker.
    
    BREAKING CHANGE: requer atualização das variáveis de ambiente.

--- driverplan/driverplan/settings.py
+++ driverplan/driverplan/settings.py
@@ -9,23 +9,29 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t^4tjb1u2-^^ck@@x_7=wi$_gmqj=m&w94v&xsvksuti*ud5x1'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-t^4tjb1u2-^^ck@@x_7=wi$_gmqj=m&w94v&xsvksuti*ud5x1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+# getenv pega um determinado valor em arquivo .env
+# abaixo, trata-se o allowed_hosts segundo suas definições: separadas por virgulas e desconsiderandos espaços vazios.
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
+    if h.strip()
+]
 
 
 # Application definition
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'driverplan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'change-me'),
+        'NAME': os.getenv('POSTGRES_DB', 'change-me'),
+        'USER': os.getenv('POSTGRES_USER', 'change-me'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'change-me'),
+        'HOST': os.getenv('POSTGRES_HOST', 'change-me'),
+        'PORT': os.getenv('POSTGRES_PORT', 'change-me'),
     }
 }
 
@@ -104,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -120,7 +130,13 @@ GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', 'AIzaSyBCrkOnN_VK1ya_FzAy
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# /data/web/static
+STATIC_ROOT = DATA_DIR / 'static'
+
+MEDIA_URL = '/media/'
+# /data/web/media
+MEDIA_ROOT = DATA_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
